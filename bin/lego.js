b50eb8bf58f2d4976473024956d807d8175a5ed4
weilai <weilai.wl@alibaba-inc.com>
2016-06-29 18:54:16 +0800

    pack

--- bin/lego.js
+++ bin/lego.js
@@ -4,6 +4,7 @@
 const path = require('path')
 const Liftoff = require('liftoff')
 const chalk = require('chalk')
+const moment = require('moment')
 const pkg = require('../package.json')
 const argv = require('minimist')(process.argv.slice(2))
 const sh = require('shelljs')
@@ -40,11 +41,14 @@ cli.launch({
     case 'build':
       const buildConfig = path.resolve(__dirname, '../config/webpack.config.build.js')
       const watch = argv.watch ? '--watch' : ''
-      ret = sh.exec(`webpack -d --config=${buildConfig} ${watch} --colors --progress`, { silent: false })
+      sh.exec(`webpack -d --config=${buildConfig} ${watch} --colors --progress`, { silent: false })
       break
     case 'release':
       const distConfig = path.resolve(__dirname, '../config/webpack.config.dist.js')
-      ret = sh.exec(`webpack -d --config=${distConfig} --colors --progress`, { silent: false })
+      sh.exec(`webpack -d --config=${distConfig} --colors --progress`, { silent: false })
+      sh.exec(`cd dist && npm i --production`, { silent: false })
+      const proj = require(path.join(process.cwd(), '/package.json'))
+      const packName = [proj.name, 'v'+proj.version, moment().format('YYYYMMDDHHmm')].join('_')
       break
     case 'clean':
       if (/win32/.test(process.platform)) {
